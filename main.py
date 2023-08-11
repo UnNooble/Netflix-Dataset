cefe4e1e88d5fd46c5e30dbb5d610efab1327323
Mark Liu <112778262+UnNooble@users.noreply.github.com>
2023-08-11 10:13:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ st.title("Netflix Userbase Data Information Collab")
 #adding discription to your website
 st.header('Bari, Mark, & Ojas')
 
-st.write("Hi! My name is Barindra Surjaudaja and I will be attending Grade 10. Furthermore, I've had quite a bit of experienece with python coding using IDLE (eg: selection statements, loops, if statements, list, etc). However, this AI course has definately enhanced my knowledge regarding AI and the extraction of data and information to form data charts. This knowledge, without a doubt, will be very useful!")
+st.write("Hi! My name is Barindra Surjaudaja and I will be attending Grade 10. Furthermore, I've had quite a bit of experience with python coding using IDLE (eg: selection statements, loops, if statements, lists, etc). However, this AI course has definately enhanced my knowledge regarding AI and the extraction of data and information to form data charts. This knowledge, without a doubt, will be very useful!")
 
 st.write("Hi, I'm Ojas, I'm currently in high school. I had a bit of prior experience in coding before joining AI camp, but I plan to deepen my understanding and learn some new skills.")
 
@@ -24,7 +24,7 @@ st.write("Hello! My name is Mark Liu. Currently, I am a high school student. I h
 
 st.header('Context to the Dataset')
 
-st.write("These charts are formed to show numerous different data informative charts mainly revolving around the topic of the Netflix subscriptions. Within this replit, we have charts outputting information of which subscription is most popular, the different gender-wise interests of different subscriptions, the country-wise choices of subscriptions, and shows which subscription is most profitable. In addition to that, we have a few other charts that also shows information regarding Netflix itself: which country generates most income for the company and which is the most used device to watch Netflix. All these charts are extremely important to propose a sense of what the company, Netflix, is like.")
+st.write("These charts are formed to show numerous different data informative charts mainly revolving around the topic of the Netflix subscriptions. Within this split, we have charts outputting information on which subscription is most popular, the different gender-wise interests of different subscriptions, the country-wise choices of subscriptions, and shows which subscription is most profitable. In addition to that, we have a few other charts that also shows information regarding Netflix itself: which country generates most income for the company and which is the most used device to watch Netflix. All these charts are extremely important to propose a sense of what the company, Netflix, is like.")
 
 
 
@@ -33,12 +33,12 @@ st.header("Dataset")
 image = Image.open("Subscription Type in Different Country.png")
 st.image(image,caption="Bar graph showing the number of various subscription types from different countries.")
 
-st.write("The bar graph provides a detailed visualization of the three primary Netflix subscription tiers: Basic, Standard, and Premium, across various countries. Interestingly, each country exhibits a distinct preference pattern, with variations in the distribution of these subscription types. This variation highlights the differing consumption tendencies of Netflix's subscription in different geographical regions.")
+st.write("The bar graph provides a detailed visualization of the three primary Netflix subscription tiers: Basic, Standard, and Premium, across various countries. Interestingly, each country has a distinct preference pattern, with variations in the distribution of these subscription types. This variation highlights the differing consumption tendencies of Netflix's subscription in different geographical regions.")
 
 image = Image.open("Device Percentage.png")
-st.image(image,caption="Bar graph showing the various device types used to watch Netflix.")
+st.image(image,caption="Pie chart showing the various device types used to watch Netflix.")
 
-st.write("The bar graph provides a visual representation of the diverse range of device types utilized to access Netflix. Notably, each device category — from smartphones to laptops — displays an similar user count, suggesting a balanced preference or accessibility among Netflix's audience across all platforms.")
+st.write("The pie chart provides a visual representation of the diverse range of device types utilized to access Netflix. From smartphones to laptops, displaying a similar user count, suggesting a balanced preference or accessibility among Netflix's audience across all platforms.")
 
 image = Image.open("chart that shows gender-wise interest of subscription type.png")
 st.image(image, caption="This graph illustrates which subscription is most popular among the 2 genders")
